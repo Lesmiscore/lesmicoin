1810ec09c844af79dafc50a3c34e5793ca040a8b
nao20010128nao <nao20010128@gmail.com>
2018-10-03 23:09:38 +0900

    シードノード追加
    動くといいな

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -123,7 +123,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("illegal.localhost");
+        vSeeds.emplace_back("lesmicoin.moe.hm");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,23); // A
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,37); // G
